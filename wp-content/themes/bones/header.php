e5bffd75cc436ae8308737a71d7e4c53a34bd2ec
Paul Strain <pillarcreation@gmail.com>
2014-11-30 14:50:08 -0800

    removed mix up in duplicate bones folders

--- wp-content/themes/bones/header.php
+++ wp-content/themes/bones/header.php
@@ -10,7 +10,8 @@
 
 		<?php // force Internet Explorer to use the latest rendering engine available ?>
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-
+        
+        <link rel="stylesheet" type="text/css" href="/wp-content/themes/bones/library/css/my-styles.css">
 		<title><?php wp_title(''); ?></title>
 
 		<?php // mobile meta (hooray!) ?>
@@ -37,6 +38,9 @@
 		<?php // drop Google Analytics Here ?>
 		<?php // end analytics ?>
 
+<meta name='viewport' content='width=device-width, initial-scale=1.0' />
+		<meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' />
+
 	</head>
 
 	<body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
@@ -45,22 +49,56 @@
 
 			<header class="header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
 
+
+
 				<div id="inner-header" class="wrap cf">
 
 					<?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
-					<p id="logo" class="h1" itemscope itemtype="http://schema.org/Organization"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
+						<a href="<?php echo home_url(); ?>" rel="nofollow"><img id="MyLogo" src="http://terraintechnicalservices.ca/wp-content/uploads/terrain-technical-services-logo.png" /></a>
+						<ul class="menubar">
+  <li>
+    <a href="#">File</a>
+    <ul>
+      <li><a href="#">Open</a></li>
+      <li><a href="#">Save</a></li>
+      <li><a href="#">Save as&#8230;</a></li>
+      <li><a href="#">Close</a></li>
+      <li class="separator"></li>
+      <li><a href="#">Exit</a></li>
+    </ul>
+  </li>
+  <li>
+    <a href="#">Edit</a>
+    <ul>
+      <li><a href="#">Cut</a></li>
+      <li><a href="#">Copy</a></li>
+      <li><a href="#">Paste</a></li>
+    </ul>
+  </li>
+  <li>
+    <a href="#">Help</a>
+    <ul>
+      <li><a href="#">About</a></li>
+    </ul>
+  </li>
+</ul>
 
 					<?php // if you'd like to use the site description you can un-comment it below ?>
 					<?php // bloginfo('description'); ?>
+
+                                 
+
+
+
 
 
 					<nav role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
 						<?php wp_nav_menu(array(
     					         'container' => false,                           // remove nav container
-    					         'container_class' => 'menu cf',                 // class of container (should you choose to use it)
+    					         'container_class' => 'menubar',                 // class of container (should you choose to use it)
     					         'menu' => __( 'The Main Menu', 'bonestheme' ),  // nav name
     					         'menu_class' => 'nav top-nav cf',               // adding custom nav class
-    					         'theme_location' => 'main-nav',                 // where it's located in the theme
+    					         'theme_location' => 'menubar',                 // where it's located in the theme
     					         'before' => '',                                 // before the menu
         			               'after' => '',                                  // after the menu
         			               'link_before' => '',                            // before each link
@@ -68,6 +106,8 @@
         			               'depth' => 0,                                   // limit the depth of the nav
     					         'fallback_cb' => ''                             // fallback function (if there is one)
 						)); ?>
+
+
 
 					</nav>
 
